75afa97900fc827a75489660e5604b8174ca2e91
lucas.lima <lucas@tew.one>
2025-04-25 16:55:55 -0300

    WIP - feat: add oauth social login using socialite

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\EnsureApiAuthenticated;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,6 +12,9 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('google')->stateless()->redirect();
+});
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
